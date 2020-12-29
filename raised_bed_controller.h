a974ebe54441fb3ef09e1a31e4bb108f428967e6
heidenbloed <git@heidenblut.eu>
2020-12-29 23:23:44 +0100

    Add inital raised_bed_controller code.

---
+++ raised_bed_controller.h
@@ -0,0 +1,41 @@
+/*
+ * raised_bed_controller.h
+ *
+ *  Created on: Dec 29, 2020
+ *      Author: tim
+ */
+
+#ifndef RAISED_BED_CONTROLLER_H_
+#define RAISED_BED_CONTROLLER_H_
+
+#include "Arduino.h"
+#include "SPI.h"
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <DS3231.h>
+
+void initTft();
+void handleRadioMsg(long nowUnix);
+void logMoistureValue();
+void updatePumpVoltage();
+void initWatering(bool manual);
+void wateringLoop();
+void endWatering();
+void updateCurrentTankStatus(int currentPumpVal);
+void checkIfTankWasRefilled();
+void updateClockAndWateringTime(RTCDateTime now, long nextWateringTime);
+void sendViaRadio(String msg);
+void updateTftStatus(String status);
+void updateTftWatering(String nextWatering);
+void updateTftMoisture(String moistureLevel);
+void updateTftPumpVoltage(String pumpVoltage);
+void updateTftTime(String timeStr);
+void tankRefillISR();
+void manualWateringISR();
+
+
+#endif /* RAISED_BED_CONTROLLER_H_ */
